42a985bcfd8bdb6b23a077b4ecd06f1f6ce110e4
Parvinder Kaur <744382@student.tor.loc>
2019-04-11 12:16:35 -0400

    last commit

---
+++ mapsscript.js
@@ -0,0 +1,27 @@
+var map, map1, map2, map3;
+function initialize(){
+    var mapOptions1 = {
+        zoom:2,
+        center: { lat: 40.758904, lng:-73.985128}
+    };
+    var mapOptions2 = {
+        zoom:10,
+        center: { lat: 40.758904, lng:-73.985128}
+    };
+    var mapOptions3 = {
+        zoom:18,
+        center: { lat: 40.758904, lng:-73.985128}
+    };
+
+    var mapOptions= {
+        zoom:12,
+        center: new google.maps.LatLng (40.758904, -73.985128)
+    };
+
+    map =new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+    map1 = new google.maps.Map(document.getElementById('map_canvas1'), mapOptions1);
+    map1 = new google.maps.Map(document.getElementById('map_canvas2'), mapOptions2);
+    map1 = new google.maps.Map(document.getElementById('map_canvas3'), mapOptions3);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+    
